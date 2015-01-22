f0f55c081e69d4540c887e16950d259907e66495
Shak <shaker@bellycard.com>
2015-01-21 18:32:25 -0600

    keep a count of the routing keys and print them out when you ctrl-c out of thumper

--- lib/thumper/cli.rb
+++ lib/thumper/cli.rb
@@ -19,6 +19,7 @@ module Thumper
 
         channel = connection.create_channel
         queue   = channel.queue 'thumper', auto_delete: true
+        routing_keys_count = {}
         running = true
 
         Signal.trap('INT') { running = false }
@@ -26,7 +27,8 @@ module Thumper
         while running do
           queue.bind(channel.topic(name), routing_key: '#')
           queue.subscribe(manual_ack: true) do |delivery_info, metadata, payload|
-            puts "delivery_info: #{delivery_info[:routing_key]}"
+            routing_key = delivery_info[:routing_key]
+            puts "delivery_info: #{routing_key}"
             puts "metadata: #{metadata}"
             if metadata[:content_type] == 'application/json'
               json = JSON.parse(payload)
@@ -35,11 +37,28 @@ module Thumper
               puts "payload: #{payload}"
             end
             puts "\n"
+
+            increment_routing_key_counts(routing_keys_count, routing_key)
           end
+        end
+
+        puts "\n\n-- Routing Key Counts --"
+        routing_keys_count.each do |key, count|
+          puts "#{key}: #{count}"
         end
 
         connection.close
       end
+
+      private
+
+        def increment_routing_key_counts(counts_hash, key)
+          if counts_hash.has_key? key
+            counts_hash[key] += 1
+          else
+            counts_hash[key] = 1
+          end
+        end
     end
   end
 end
